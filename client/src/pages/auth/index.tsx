dd01c426c57f07053197966928c4fb682e296604
Alexander Lead <a.lead@yandex.ru>
2024-01-25 11:38:46 +0300

    feat(client): login form

--- client/src/pages/auth/index.tsx
+++ client/src/pages/auth/index.tsx
@@ -1,9 +1,120 @@
-import React from 'react'
+import React, { SyntheticEvent, useState } from 'react'
+import axios from "axios";
+
+import "./styles.css";
+import { Response } from 'express';
+import { UserErrors } from '../../errors';
+import { useCookies } from 'react-cookie';
+
+const Register = () => {
+
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const handleSubmit = async (event: SyntheticEvent) => {
+    event.preventDefault();
+    try{
+
+      await axios.post("http://localhost:3001/user/register/",
+      {
+        username,
+        password
+      });
+      console.log("registration Completed!");
+    } catch (error) {
+      if(error?.response?.data?.type === UserErrors.USERNAME_ALREADY_EXISTS) {
+          console.log("Error: Username already in use.")
+        } else {
+          console.log("Error: Something went wrong.")
+      }
+    }
+
+  }
+
+  return (
+    <div className='auth-container'>
+      <form onSubmit={handleSubmit}>
+        <h2>Register</h2>
+        <div className='form-group'>
+          <label htmlFor="username">Username: </label>
+          <input type="text" id="username" value={username} onChange={(event) => setUsername(event.target.value)} />
+        </div>
+        <div className='form-group'>
+          <label htmlFor="password">Password: </label>
+          <input type="password" id="password" value={password} onChange={(event) => setPassword(event.target.value)} />
+        </div>
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  )
+}
+
+const Login = () => {
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const [_, setCookies] = useCookies(["access_tocken"]);
+
+  const handleSubmit = async (event: SyntheticEvent) => {
+    event.preventDefault();
+    try{
+
+      const result = await axios.post("http://localhost:3001/user/login/",
+      {
+        username,
+        password
+      });
+     
+      setCookies("access_tocken", result.data.token );
+      localStorage.setItem("userId", result.data.userId);
+
+    } catch (error) {
+
+      let errorMessage: string = "";
+
+      switch(error.response.data.type) {
+        case UserErrors.NO_USER_FOUND:
+          errorMessage = "User doesn't exists.";
+          break;
+        case UserErrors.WRONG_CREDENTIALS:
+          errorMessage = "Wrong user/password combination";
+          break;
+        default:
+          errorMessage = "Something went wrong";
+      }
+
+      console.log("ERROR: " + errorMessage);
+
+
+    }
+
+  }
+
+  return (
+    <div className='auth-container'>
+      <form onSubmit={handleSubmit}>
+        <h2>Login</h2>
+        <div className='form-group'>
+          <label htmlFor="username">Username: </label>
+          <input type="text" id="username" value={username} onChange={(event) => setUsername(event.target.value)} />
+        </div>
+        <div className='form-group'>
+          <label htmlFor="password">Password: </label>
+          <input type="password" id="password" value={password} onChange={(event) => setPassword(event.target.value)} />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  )
+}
 
 const AuthPage = () => {
   return (
-    <div>Auth</div>
+    <div className='auth'>
+      <Register /> <Login />
+    </div>
   )
 }
 
 export default AuthPage
+
