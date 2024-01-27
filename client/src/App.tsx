44514ca6d1c7dc3c00ca08f2f41b1df0efbd19cc
Alexander Lead <a.lead@yandex.ru>
2024-01-27 14:22:30 +0300

    feat (client): add context, add to cart func

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,26 @@
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
 import Navbar from './components/navbar';
 import ShopPage from "./pages/shop";
 import AuthPage from "./pages/auth";
 import CheckoutPage from "./pages/checkout";
 import PurchasedItemsPage from "./pages/purchased-items";
+import { ShopContextProvider } from "./context/shop-context";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Navbar/>
-        <Routes>
-          <Route path="/" element={ <ShopPage/> }/>
-          <Route path="/auth" element={ <AuthPage/>}/>
-          <Route path="/checkout" element={<CheckoutPage/>}/>
-          <Route path="/purchased-items" element={<PurchasedItemsPage/>}/>
-        </Routes>
+        <ShopContextProvider>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<ShopPage />} />
+            <Route path="/auth" element={<AuthPage />} />
+            <Route path="/checkout" element={<CheckoutPage />} />
+            <Route path="/purchased-items" element={<PurchasedItemsPage />} />
+          </Routes>
+        </ShopContextProvider>
       </Router>
-      
     </div>
   );
 }
--- client/src/pages/shop/Product.tsx
+++ client/src/pages/shop/Product.tsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { IProduct } from '../../models/interfaces'
+import { IShopContext, ShopContext } from '../../context/shop-context';
 
 interface Props {
   product: IProduct;
@@ -8,6 +9,10 @@ interface Props {
 const Product = (props: Props) => {
 
   const {_id, productName, description, price, stockQuantity, imageURL } = props.product;
+
+  const {addToCart, getCartItemCount} = useContext<IShopContext>(ShopContext)
+
+  const countProduct = getCartItemCount(_id);
 
   return (
     <div className='product'> 
@@ -20,6 +25,10 @@ const Product = (props: Props) => {
         <p>
           ${price}
         </p>
+      </div>
+      <button className='add-to-cart-btn' onClick={()=>addToCart(_id)}>Add To Cart {countProduct > 0 && <p>{countProduct}</p>}</button>
+      <div className="stock-quantity">
+        {stockQuantity === 0 && <h2>Out of Stock</h2>}
       </div>
     </div>
   )
